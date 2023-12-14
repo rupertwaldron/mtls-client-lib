8510f526288aa597b4b4fe22084a2ff3325a43b9
rupertwaldron <rupert.waldron@yahoo.co.uk>
2023-12-14 08:55:58 +0000

    initial commit for library

---
+++ src/main/java/com/ruppyrup/mtlsclientlib/MtlsConfiguration.java
@@ -0,0 +1,20 @@
+package com.ruppyrup.mtlsclientlib;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.ssl.SslBundles;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+@ConditionalOnProperty(name = "mtls.enabled", havingValue = "true")
+public class MtlsConfiguration {
+
+    @Bean("mtlsRestTemplate")
+    public RestTemplate mtlsRestTemplate(RestTemplateBuilder restTemplateBuilder, SslBundles sslBundles) {
+        return restTemplateBuilder.setSslBundle(sslBundles.getBundle("emoji-api")).build();
+    }
+
+}
---
+++ src/test/java/com/ruppyrup/mtlsclientlib/MtlsClientLibApplicationTests.java
@@ -0,0 +1,25 @@
+package com.ruppyrup.mtlsclientlib;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootTest(
+properties = {
+        "mtls.enabled=true",
+        }
+)
+class MtlsClientLibApplicationTests {
+
+    @Autowired
+    RestTemplate mtlsRestTemplate;
+
+    @Test
+    void contextLoads() {
+        String restTemplateName = mtlsRestTemplate.getClass().getSimpleName();
+        Assertions.assertEquals("RestTemplate", restTemplateName);
+    }
+
+}
